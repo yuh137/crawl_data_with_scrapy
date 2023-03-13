6e0575fe70de89501ba4463def97aa2f6e096d7f
nqhuy137 <nquochuy137@gmail.com>
2023-03-13 22:30:04 +0700

    built spider

---
+++ stack/items.py
@@ -0,0 +1,15 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+from scrapy.item import Field, Item
+
+
+class StackItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    url = Field()
+    title = Field()
---
+++ stack/pipelines.py
@@ -0,0 +1,39 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pymongo 
+from scrapy import settings
+from scrapy.exceptions import DropItem
+import logging
+
+
+class MongoDBPipeline(object):
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+        self.logger = logging.getLogger(__name__)
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert_one(dict(item))
+            self.logger.debug("Question added to MongoDB database!")
+        return item
+
+
+# class StackPipeline:
+#     def process_item(self, item, spider):
+#         return item
